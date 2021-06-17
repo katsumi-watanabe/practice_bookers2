b7c94f666789a50e97dd56f2ec7544b8b6937f4f
katsumi-watanabe <ktm981124@gmail.com>
2021-06-17 13:34:34 +0000

    third commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def create
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#top"
+  get "/home/about", to: "home#about"
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
 end
