7314b5edd9e7f8887e72362e89788571c739ed89
katsumi-watanabe <ktm981124@gmail.com>
2021-06-19 10:32:26 +0000

    end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,28 @@
 class BooksController < ApplicationController
 
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to root_path
   end
 
   def edit
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def show
   end
-  
+
   def destroy
   end
 
+  private
+  def book_params
+    params.require(:book).permit(:title, :book)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
 
   def create
+    @user = User.new(user_params)
   end
 
   def edit
   end
 
   def index
+    @user = User.new
+    @users = User.all
   end
 
   def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
